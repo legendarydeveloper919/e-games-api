31f5c4aae08d1b04c4f985dcf2c1c6782d0abd97
legendarydeveloper919 <peterandersen919@gmail.com>
2023-06-24 15:10:06 -0300

    feat: Adding validation in model coupon

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -5,6 +5,7 @@ class Coupon < ApplicationRecord
   validates :code, presence: true, uniqueness: { case_sensitive: false }
   validates :discount_value, presence: true, numericality: { greater_than: 0 }
   validates :max_use, presence: true, numericality: { greater_than: 0 }
+  validates :due_date, presence: true, future_date: true
 
   enum status: { active: 1, inactive: 0 }
 end
