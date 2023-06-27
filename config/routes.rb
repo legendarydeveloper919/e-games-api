20846d57724384e648fd128d6b9e0d0d857c1bdc
legendarydeveloper919 <peterandersen919@gmail.com>
2023-06-26 22:57:33 -0300

    feat: Adding router to categories

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@
 
 Rails.application.routes.draw do
   mount_devise_token_auth_for "User", at: "auth/v1/user"
-  
+  namespace :admin do
+    namespace :v1 do
+      resources :categories
+    end
+  end  
 end
