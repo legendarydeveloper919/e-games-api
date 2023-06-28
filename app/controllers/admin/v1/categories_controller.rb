cf47d38ed3ce3c3bd6b99b2205feb8f18e664f33
legendarydeveloper919 <peterandersen919@gmail.com>
2023-06-27 22:29:47 -0300

    feat: Adding action create category and view show

--- app/controllers/admin/v1/categories_controller.rb
+++ app/controllers/admin/v1/categories_controller.rb
@@ -6,6 +6,23 @@ module Admin
       def index
         @categories = Category.all
       end
+
+      def create
+        @category = Category.new
+        @category.attributes = category_params
+        @category.save!
+        render(:show)
+      rescue
+        render_error(fields: @category.errors.messages)
+      end
+
+      private
+
+      def category_params
+        return {} unless params.has_key?(:category)
+
+        params.require(:category).permit(:id, :name)
+      end
     end
   end
 end
