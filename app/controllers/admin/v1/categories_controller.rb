28e80f743060827a45a6e83bd6aa83dac09aecff
legendarydeveloper919 <peterandersen919@gmail.com>
2023-06-29 00:12:10 -0300

    feat: Adding action destroy category

--- app/controllers/admin/v1/categories_controller.rb
+++ app/controllers/admin/v1/categories_controller.rb
@@ -19,6 +19,13 @@ module Admin
         save_category!
       end
 
+      def destroy
+        @category = Category.find(params[:id])
+        @category.destroy!
+      rescue
+        render_error(fields: @category.errors.messages)
+      end
+
       private
 
       def category_params
