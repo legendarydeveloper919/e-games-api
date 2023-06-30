d9029b871dfcdf1ab1a22533b726f10e2ea54f52
legendarydeveloper919 <peterandersen919@gmail.com>
2023-06-29 23:06:06 -0300

    feat: Adding access restriction'

---
+++ spec/requests/admin/v1/categories/unauthenticated_requests_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+RSpec.describe "Admin v1 categories without authentication", type: :request do
+  context "GET /catgories" do
+    let(:url) { "/admin/v1/categories" }
+    let!(:categories) { create_list(:category, 5) }
+
+    before(:each) { get url }
+  end
+
+  context "POST /categories" do
+    let(:url) { "/admin/v1/categories" }
+
+    before(:each) { post url }
+
+    include_examples "unauthenticated access"
+  end
+
+  context "PATCH /categories/:id" do
+    let(:category) { create(:category) }
+    let(:url) { "/admin/v1/categories/#{category.id}" }
+
+    before(:each) { patch url }
+
+    include_examples "unauthenticated access"
+  end
+
+  context "DELTE /categories" do
+    let!(:category) { create(:category) }
+    let(:url) { "/admin/v1/categories/#{category.id}" }
+
+    before(:each) { delete url }
+
+    include_examples "unauthenticated access"
+  end
+end
