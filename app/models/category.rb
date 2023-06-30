bf0afde89f21c4fe5bddf6f2d95c1b20f4b6addc
legendarydeveloper919 <peterandersen919@gmail.com>
2023-06-30 16:11:58 -0300

    test: Adding conceren tests

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Category < ApplicationRecord
+  include NameSearchable
+
   has_many :product_categories, dependent: :destroy
   has_many :products, through: :product_categories
 
