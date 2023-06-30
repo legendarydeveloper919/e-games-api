aa3d70c498f6c987c5bc90be23928ae34b83e420
legendarydeveloper919 <peterandersen919@gmail.com>
2023-06-30 16:11:05 -0300

    feat: Create concern name searchale

---
+++ app/models/concerns/name_searchable.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module NameSearchable
+  extend ActiveSupport::Concern
+
+  included do
+    scope :search_by_name, ->(value) do
+      self.where("name ILIKE ?", "%#{value}%")
+    end
+  end
+end
