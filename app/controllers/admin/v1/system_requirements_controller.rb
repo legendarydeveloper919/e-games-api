baf75a25696dc5ebef83f984fd602a532edc097f
legendarydeveloper919 <peterandersen919@gmail.com>
2023-07-29 16:40:11 -0300

    fix: Passing create method into the class

--- app/controllers/admin/v1/system_requirements_controller.rb
+++ app/controllers/admin/v1/system_requirements_controller.rb
@@ -6,27 +6,27 @@ module Admin
       def index
         @system_requirements = SystemRequirement.all
       end
-    end
 
-    def create
-      @system_requirement = SystemRequirement.new
-      @system_requirement.attributes = system_requirement_params
-      save_system_requirement!
-    end
+      def create
+        @system_requirement = SystemRequirement.new
+        @system_requirement.attributes = system_requirement_params
+        save_system_requirement!
+      end
 
-    private
+      private
 
-    def system_requirement_params
-      return {} unless params.has_key?(:system_requirement)
-      params.require(:system_requirement).permit(:id, :name, :operational_system, :storage,
-                                                 :processor, :memory, :video_board)
-    end
+      def system_requirement_params
+        return {} unless params.has_key?(:system_requirement)
+        params.require(:system_requirement).permit(:id, :name, :operational_system, :storage,
+                                                   :processor, :memory, :video_board)
+      end
 
-    def save_system_requirement!
-      @system_requirement.save!
-      render :show
-    rescue
-      render_error fields: @system_requirement.errors.messages
+      def save_system_requirement!
+        @system_requirement.save!
+        render :show
+      rescue
+        render_error fields: @system_requirement.errors.messages
+      end
     end
   end
 end
