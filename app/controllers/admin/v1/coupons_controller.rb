b590b3a815d2bc8a3c150f5afe54b27b2de0bd3c
legendarydeveloper919 <peterandersen919@gmail.com>
2023-07-31 22:47:39 -0300

    feat: Adding action create coupon controller

--- app/controllers/admin/v1/coupons_controller.rb
+++ app/controllers/admin/v1/coupons_controller.rb
@@ -6,6 +6,28 @@ module Admin
       def index
         @coupons = Coupon.all
       end
+
+      def create
+        @coupon = Coupon.new
+        @coupon.attributes = coupon_params
+        save_coupon!
+      end
+
+      private
+
+      def coupon_params
+        return {} unless params.has_key?(:coupon)
+        params.require(:coupon).permit(:id, :name, :code, :status,
+                                       :discount_value, :max_use,
+                                       :due_date)
+      end
+
+      def save_coupon!
+        @coupon.save!
+        render :show
+      rescue
+        render_error fields: @coupon.errors.messages
+      end
     end
   end
 end
