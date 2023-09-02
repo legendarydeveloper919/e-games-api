d811b05b8545583b115572177c4d8b136067ca80
legendarydeveloper919 <peterandersen919@gmail.com>
2023-09-01 23:45:52 -0300

    test: Adding tests to create user

--- spec/requests/admin/v1/users/users_spec.rb
+++ spec/requests/admin/v1/users/users_spec.rb
@@ -24,4 +24,51 @@ RSpec.describe "Admin::V1::Users", type: :request do
       expect(response).to have_http_status(:ok)
     end
   end
+
+  context "POST /users" do
+    let(:url) { "/admin/v1/users" }
+
+    context "with valid params" do
+      let(:user_params) { { user: attributes_for(:user) }.to_json }
+
+      it "adds a new user" do
+        expect do
+          post url, headers: auth_header(login_user), params: user_params
+        end.to change(User, :count).by(1)
+      end
+
+      it "returns last added user" do
+        post url, headers: auth_header(login_user), params: user_params
+        expected_user = User.last.as_json(only: %i(id name email profile))
+        expect(body_json["user"]).to eq expected_user
+      end
+
+      it "returns success status" do
+        post url, headers: auth_header(login_user), params: user_params
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "with invalid params" do
+      let(:user_invalid_params) do
+        { user: attributes_for(:user, name: nil) }.to_json
+      end
+
+      it "does not add a new user" do
+        expect do
+          post url, headers: auth_header(login_user), params: user_invalid_params
+        end.to_not change(User, :count)
+      end
+
+      it "returns error message" do
+        post url, headers: auth_header(login_user), params: user_invalid_params
+        expect(body_json["errors"]["fields"]).to have_key("name")
+      end
+
+      it "returns unprocessable_entity status" do
+        post url, headers: auth_header(login_user), params: user_invalid_params
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
 end
