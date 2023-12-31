2e9363d8e4646ccf0f99a7bfbb585b4202a31b1c
legendarydeveloper919 <peterandersen919@gmail.com>
2023-12-30 22:48:06 -0300

    feat: Adding  product controller index action

---
+++ app/controllers/admin/v1/products_controller.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Admin
+  module V1
+    class ProductsController < ApiController
+      def index
+        @products = load_products
+      end
+
+      private
+
+      def load_products
+        permitted = params.permit({ search: :name }, { order: {} }, :page, :length)
+        Admin::ModelLoadingService.new(Product.all, permitted).call
+      end
+    end
+  end
+end
