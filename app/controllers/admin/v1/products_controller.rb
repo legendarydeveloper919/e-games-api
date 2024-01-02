197e9caa5c6b8e450791a7709dd602c75bb0301e
legendarydeveloper919 <peterandersen919@gmail.com>
2024-01-01 23:29:56 -0300

    feat: Adding  product controller create action

--- app/controllers/admin/v1/products_controller.rb
+++ app/controllers/admin/v1/products_controller.rb
@@ -7,7 +7,40 @@ module Admin
         @products = load_products
       end
 
+      def create
+        run_service
+      rescue Admin::ProductSavingService::NotSavedProductError
+        render_error(fields: @saving_service.errors)
+      end
+
       private
+
+      def product_params
+        return {} unless params.has_key?(:product)
+        permitted_params = params.require(:product).permit(:id, :name,
+                                                           :description, :image,
+                                                           :price, :productable,
+                                                           :status, category_ids: [])
+        permitted_params.merge(productable_params)
+      end
+
+      def productable_params
+        productable_type = params[:product][:productable] || @product&.productable_type&.underscore
+        return unless productable_type.present?
+        productable_attributes = send("#{productable_type}_params")
+        { productable_attributes: productable_attributes }
+      end
+
+      def game_params
+        params.require(:product).permit(:mode, :release_date, :developer, :system_requirement_id)
+      end
+
+      def run_service(product = nil)
+        @saving_service = Admin::ProductSavingService.new(product_params.to_h, @product)
+        @saving_service.call
+        @product = @saving_service.product
+        render :show
+      end
 
       def load_products
         permitted = params.permit({ search: :name }, { order: {} }, :page, :length)
