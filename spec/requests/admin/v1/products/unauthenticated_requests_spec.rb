059343de4917a9b61c098af56f772f1ec18fb676
legendarydeveloper919 <peterandersen919@gmail.com>
2024-01-04 22:31:22 -0300

    test: Adding unauthenticated tests to product show

--- spec/requests/admin/v1/products/unauthenticated_requests_spec.rb
+++ spec/requests/admin/v1/products/unauthenticated_requests_spec.rb
@@ -26,4 +26,13 @@ RSpec.describe "Admin v1 Product without authentication", type: :request do
 
     include_examples "unauthenticated access"
   end
+
+  context "PATCH /products/:id" do
+    let(:product) { create(:product) }
+    let(:url) { "/admin/v1/products/#{product.id}" }
+
+    before(:each) { patch url }
+
+    include_examples "unauthenticated access"
+  end
 end
