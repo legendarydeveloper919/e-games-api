5d3927b4e50671fa416176cac069f055ad465d47
legendarydeveloper919 <peterandersen919@gmail.com>
2024-01-22 23:29:01 -0300

    feat: Adding attibute featured

--- app/controllers/admin/v1/products_controller.rb
+++ app/controllers/admin/v1/products_controller.rb
@@ -41,7 +41,7 @@ module Admin
         permitted_params = params.require(:product).permit(:id, :name,
                                                            :description, :image,
                                                            :price, :productable,
-                                                           :status, category_ids: [])
+                                                           :status, :featured, category_ids: [])
         permitted_params.merge(productable_params)
       end
 
