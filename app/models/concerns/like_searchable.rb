10b74cda5a27a643d1bdaacfd4cf640756039959
legendarydeveloper919 <peterandersen919@gmail.com>
2024-01-26 07:32:50 -0300

    feat: Adding like searchable

---
+++ app/models/concerns/like_searchable.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module LikeSearchable
+  extend ActiveSupport::Concern
+
+  included do
+    scope :like, ->(field, value) do
+      self.where(self.arel_table[field].matches("%#{value}%"))
+    end
+  end
+end
