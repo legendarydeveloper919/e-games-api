55708189acd7545de158406f69640ba9653f2c32
legendarydeveloper919 <peterandersen919@gmail.com>
2024-01-30 22:25:55 -0300

    feat: Adding products controller storefront

---
+++ app/controllers/storefront/v1/products_controller.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Storefront::V1
+  class ProductsController < ApplicationController
+    def index
+      @products = Storefront::ProductsFilterService.new(search_params).call
+    end
+
+    private
+
+    def search_params
+      params.permit(:search, :productable, :page,
+                    :length, order: {}, category_ids: [], price: [:min, :max],
+                             release_date: [:min, :max]).merge(productable: :game)
+    end
+  end
+end
