3af37d3f417b55f192f4026bf9b95c0be24e90f0
legendarydeveloper919 <peterandersen919@gmail.com>
2024-03-01 22:55:19 -0300

    fix: Fixing juno api auth

--- lib/juno_api/auth.rb
+++ lib/juno_api/auth.rb
@@ -3,13 +3,14 @@
 module JunoApi
   class Auth
     include HTTParty
+
     PATH = "/authorization-server/oauth/token"
-    base_uri "#{JUNO_AUTH_URL}"
-    LIMIT_RATE_TO_RENEW = 90
     SECONDS_TO_WAIT_PROCESSING = 0.5
+    LIMIT_RATE_TO_RENEW = 90
+
+    base_uri "#{JUNO_AUTH_URL}"
 
     attr_reader :access_token, :expires_in, :request_time
-
     private_class_method :new
 
     def self.singleton
@@ -20,12 +21,23 @@ module JunoApi
 
     private
 
+    def self.wait_until_process_is_done
+      while @processing
+        sleep SECONDS_TO_WAIT_PROCESSING
+      end
+    end
+
     def self.check_instance
       if @instance.blank? || is_about_to_expire?(@instance)
         @processing = true
         @instance = new
         @processing = false
       end
+    end
+
+    def self.is_about_to_expire?(instance)
+      expiration_rate = LIMIT_RATE_TO_RENEW / 100.0
+      instance.request_time + instance.expires_in * expiration_rate < Time.zone.now
     end
 
     def initialize
@@ -35,27 +47,16 @@ module JunoApi
       @request_time = Time.zone.now
     end
 
-    def proccess_auth!
+    def process_auth!
       body = { grant_type: "client_credentials" }
-      response = self.class.post(PATH, headers { "Authorization" => "basic #{auth_token}" }, body: body)
-      raise Error.new("Bad Request") if response.code != 200
-      respose.parsed_response
+      response = self.class.post(PATH, headers: { "Authorization" => "Basic #{auth_token}" }, body: body)
+      raise Error.new("Bad request") if response.code != 200
+      response.parsed_response
     end
 
     def auth_token
-      auth_data = Rails.application.credentials.juno.slice(:client_id, :secret)
-      Base64.strict_encode64(auth_data[:client]) + ":" + auth_data[:secret]
-    end
-
-    def self.is_about_to_expire?(instance)
-      expiration_rate = LIMIT_RATE_TO_RENEW / 100.0
-      instance.request_time + instance.expires_in * expiration_rate < Time.zone.now
-    end
-
-    def self.wait_until_process_is_done
-      while @processing
-        sleep SECONDS_TO_WAIT_PROCESSING
-      end
+      auth_data = Rails.application.credentials.juno.slice(:client, :secret)
+      Base64.strict_encode64(auth_data[:client] + ":" + auth_data[:secret])
     end
   end
 end
