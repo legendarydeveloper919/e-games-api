e349f459ef343e003b6d7df6c77f0fb7080b6ae3
legendarydeveloper919 <peterandersen919@gmail.com>
2024-03-13 22:38:47 -0300

    test: Adding tests for status in line items

--- spec/factories/line_items.rb
+++ spec/factories/line_items.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :line_item do
     quantity { 1 }
+    status { :waiting_order }
     payed_price { Faker::Commerce.price(range: 100.0..200.0) }
     order
     product
